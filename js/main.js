6cbcdc25b136b95bcd4e6d65fefcc3123ad7b9f0
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-06 04:09:17 +0500

    finished hearly

--- js/main.js
+++ js/main.js
@@ -23,3 +23,91 @@ $(document).ready(function(){
     }
   });
 });
+
+$(document).ready(function(){
+  $("#photo-carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    nav: true,
+    items: 1,
+    dots: false,
+    navText: false,
+    autoplay:true,
+    autoplayTimeout: 1000,
+    responsive: {
+        200:{
+            items: 1,
+        },
+        
+        500:{
+            items: 2,
+        },
+
+        800:{
+            items: 3,
+        },
+    },
+  });
+});
+
+// NAVBAR SHRINK
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+
+// DARK MODE
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+// BACKTOP 
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+// LOADING
+
+const loading = document.getElementById("loading");
+
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, 2000);
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
