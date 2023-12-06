228ad04e6bf8d8b46fd3767871dfdd236ea3b60b
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-06 13:59:52 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
   });
 });
 
+
 // NAVBAR SHRINK
 
 window.addEventListener("scroll", function () {
@@ -73,9 +74,9 @@ let modeBtn = document.getElementById("mode-btn");
 
 modeBtn.addEventListener("click", function () {
   if (document.body.className != "dark") {
-    this.firstElementChild.src = "images/light.svg";
+    this.firstElementChild.src = "./assets/images/light.svg";
   } else {
-    this.firstElementChild.src = "images/dark.svg";
+    this.firstElementChild.src = "./assets/images/dark.svg";
   }
   document.body.classList.toggle("dark");
 });
